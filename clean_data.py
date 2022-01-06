698e038aea4c23e6c6e0aa0a5dc1a14b874dda7e
pdrew32 <patrickmdrew@gmail.com>
2022-01-06 11:32:33 -0600

    cleaned rain and production data

--- clean_data.py
+++ clean_data.py
@@ -6,4 +6,30 @@ import pandas as pd
 clean data from excel files and save as csv
 """
 
+# load rain data
 rain = pd.read_excel('data/CONTROL_DE_LLUVIAS-1.xlsx', sheet_name=0)
+
+rain.columns = rain.columns.str.replace('Unnamed: ', 'day_')
+rain.rename(columns={rain.columns[0]:'month', rain.columns[-1]:'total'}, inplace=True)
+
+# drop rows with titles
+rain.drop(index=rain.loc[(rain.month == rain.month.unique()[0]) | (rain.month == rain.month.unique()[1]) ].index, inplace=True) # | (rain.month == rain.month.unique()[2])
+
+# get rows that correspond to years
+row_is_a_year_inds = rain.loc[np.isnan(rain.month.str.isnumeric().astype(float))].index
+
+# fill new year column with year
+for i in range(len(row_is_a_year_inds)-1):
+    rain.loc[row_is_a_year_inds[i]:row_is_a_year_inds[i+1]-1, 'year'] = rain.loc[row_is_a_year_inds, 'month'].values[i]
+
+rain.loc[row_is_a_year_inds[i]:, 'year'] = rain.loc[row_is_a_year_inds, 'month'].values[i]
+
+rain.drop(index=row_is_a_year_inds, inplace=True)
+
+
+# load production weight and year
+production = pd.read_excel('data/PRODUCCION_CAFE.xlsx')
+production = production[[production.columns[0], production.columns[5]]]
+
+production.drop(index=0, inplace=True)
+production.rename(columns={production.columns[0]:'year', production.columns[1]:'weight_kg'}, inplace=True)
