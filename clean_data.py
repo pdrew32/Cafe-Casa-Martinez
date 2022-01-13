c1d57391bed5e996032ed035072052ea274a828f
pdrew32 <patrickmdrew@gmail.com>
2022-01-13 09:18:00 -0600

    added comments to code

--- clean_data.py
+++ clean_data.py
@@ -9,22 +9,24 @@ clean data from excel files and save as csv
 # load rain data
 rain = pd.read_excel('data/CONTROL_DE_LLUVIAS-1.xlsx', sheet_name=0)
 
+# rename columns
 rain.columns = rain.columns.str.replace('Unnamed: ', 'day_')
 rain.rename(columns={rain.columns[0]:'month', rain.columns[-1]:'total'}, inplace=True)
 
-# drop rows with titles
+# drop rows that only contain titles
 rain.drop(index=rain.loc[(rain.month == rain.month.unique()[0]) | (rain.month == rain.month.unique()[1]) ].index, inplace=True) # | (rain.month == rain.month.unique()[2])
 
 # get rows that correspond to years
 row_is_a_year_inds = rain.loc[np.isnan(rain.month.str.isnumeric().astype(float))].index
 
-# fill new year column with year
+# fill a new year column with years grabbed from rows
 for i in range(len(row_is_a_year_inds)-1):
     rain.loc[row_is_a_year_inds[i]:row_is_a_year_inds[i+1]-1, 'year'] = rain.loc[row_is_a_year_inds, 'month'].values[i]
-
 rain.loc[row_is_a_year_inds[i+1]:, 'year'] = rain.loc[row_is_a_year_inds, 'month'].values[i+1]
 
+# drop rows that contained years only
 rain.drop(index=row_is_a_year_inds, inplace=True)
+
 
 
 # load production weight and year
@@ -80,17 +82,44 @@ for i in range(len(np.arange(2008, 2021))):
     tot_plants.loc[tot_plants.year == np.arange(2008, 2021)[i], 'nov_rain_cm'] = rain.loc[(rain.year == np.arange(2008, 2021)[i]) & (rain.month == 'NOVIEMBRE'), 'total'].values.astype(float)
     tot_plants.loc[tot_plants.year == np.arange(2008, 2021)[i], 'dec_rain_cm'] = rain.loc[(rain.year == np.arange(2008, 2021)[i]) & (rain.month == 'DICIEMBRE'), 'total'].values.astype(float)
 
+
+# plot # of fresh fields (zoca or plant) vs year. need to control for the fact that they're new
+tot_plants['fresh_fields'] = 0
+year_fresh = np.sort(np.append(lots.sow_year.values+3, lots.cut_year.values+2).astype(float))
+year_fresh = year_fresh[~np.isnan(year_fresh)]
+
+"""fresh_counter = 0
+for i in range(len(tot_plants)):
+    if tot_plants.loc[i, 'year'] == year_fresh[0]:
+        tot_plants.loc[i, 'fresh_fields'] = sum(year_fresh == year_fresh[fresh_counter])
+        fresh_counter += 1
+    if tot_plants.loc[i, 'year'] > year_fresh[0]:
+        tot_plants.loc[i, 'fresh_fields'] = fresh_counter + sum(year_fresh == year_fresh[fresh_counter]) + tot_plants.loc[i-1, 'fresh_fields']
+        fresh_counter += 1"""
+
+for i in range(len(year_fresh)):
+    tot_plants.loc[tot_plants.year == year_fresh[i], 'fresh_fields'] = sum(year_fresh == year_fresh[i])
+    
+for i in range(1, len(tot_plants)):
+    tot_plants.loc[i, 'fresh_fields'] = tot_plants.loc[i, 'fresh_fields'] + tot_plants.loc[i-1, 'fresh_fields']
+
+
 corr = tot_plants.corr()
 
 # print 15 most highly correlated parameters that are not auto-correlations
 print(corr.unstack().sort_values()[corr.unstack().sort_values() < 1][-15:])
 
-"""plt.scatter(tot_plants.total_rain_cm, tot_plants.prod_per_plant_kg)
+plt.scatter(tot_plants.total_rain_cm, tot_plants.prod_per_plant_kg)
 plt.ylabel('production per plant (kg)')
 plt.xlabel('total rain (cm)')
-plt.show()"""
+plt.show()
 
-plt.scatter(tot_plants.may_rain_cm, tot_plants.prod_per_plant_kg)
+plt.scatter(tot_plants.may_rain_cm, tot_plants.prod_per_plant_kg, c=tot_plants.year)
 plt.ylabel('production per plant (kg)')
 plt.xlabel('total rain in May (cm)')
+plt.show()
+
+plt.scatter(tot_plants.year, tot_plants.prod_per_plant_kg)
+plt.ylabel('production per plant (kg)')
+plt.xlabel('year')
 plt.show()
