3de1721291760e6aaada66b435047e2a7910aed3
pdrew32 <patrickmdrew@gmail.com>
2022-02-15 13:23:59 -0600

    initial commit

---
+++ constants_helper.py
@@ -0,0 +1,6 @@
+
+class c:
+    """define class to hold constants we will repeatedly use in different scripts"""
+    prod_curve_zoca = [0, 0.25, 0.8, 1.0, 0.7, 0.55]
+    prod_curve_sow = [0, 0, 0.5, 0.7, 1.0, 0.7, 0.55]
+    
