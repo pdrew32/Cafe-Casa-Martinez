78537d837c884edfcb4b9295b8a910d0aa6bcb6c
pdrew32 <patrickmdrew@gmail.com>
2022-02-22 11:10:51 -0600

    commented code and deleted obsolete code

--- ml_predict_may_rainfall.py
+++ ml_predict_may_rainfall.py
@@ -10,33 +10,62 @@ prod = pd.read_csv('data/production.csv', index_col=0)
 lots = pd.read_csv('data/lots.csv', index_col=0)
 tot_plants = pd.read_csv('data/tot_plants.csv', index_col=0)
 
+# convert production weight to loads (cargas)
+prod['cargas'] = prod.weight_kg/125
+
 rg = Generator(SFC64())
 
-april_rain = rain.loc[rain.month=='ABRIL']
-april_rain['may_rain_total'] = rain.loc[rain.month=='MAYO', 'total'].values
+# make new dataframe with may rainfall totals and year
+may_rain = rain.loc[rain.month=='MAYO', ['total', 'year']]
+may_rain.index = may_rain.year
+may_rain = may_rain.loc[np.isin(may_rain.year, prod.year.unique())]
 
-april_rain.reset_index(inplace=True, drop=True)
+# create dataframe containing rainfall totals from the beginning of each year through the end of april.
+# will train/test on that.
+dF = pd.DataFrame()
+for i in range(len(prod.year.values)):
+    a = rain.loc[rain.year == prod.year.values[i]]
+    dF[str(prod.year.values[i])] = np.hstack(a.iloc[0:4, 1:-2].values)
 
-april_rain_inds = april_rain.index.values
-rg.shuffle(april_rain_inds)
+# shuffle the years around before training/testing
+year_cols = dF.columns.values
+rg.shuffle(year_cols)
+dF = dF.loc[:, year_cols]
+dF = dF.fillna(0)
 
-april_rain = april_rain.loc[april_rain_inds]
+# split into testing and training sets
+dF_train = dF.iloc[:, :np.floor(len(year_cols)*0.7).astype(int)]
+dF_test = dF.iloc[:, np.floor(len(year_cols)*0.7).astype(int):]
 
-april_rain_train = april_rain.iloc[:np.floor(len(april_rain)*0.7).astype(int)]
-april_rain_test = april_rain.iloc[np.floor(len(april_rain)*0.7).astype(int):]
+train_x = dF_train.transpose().values
+train_y = may_rain.iloc[:np.floor(len(year_cols)*0.7).astype(int)]['total'].values
+test_x = dF_test.transpose().values
+test_y_truth = may_rain.iloc[np.floor(len(year_cols)*0.7).astype(int):]['total'].values
 
+# specify the model
 regr = linear_model.LinearRegression()
 
-april_rain_train_x = april_rain_train.loc[:, april_rain_train.columns[1]:april_rain_train.columns[30]].values
-april_rain_train_y = april_rain_train.loc[:, april_rain_train.columns[32]]
-april_rain_test_x = april_rain_test.loc[:, april_rain_test.columns[1]: april_rain_train.columns[30]].values
-april_rain_test_y = april_rain_test.loc[:, april_rain_train.columns[32]].values
+regr.fit(train_x, train_y)
 
-regr.fit(april_rain_train_x, april_rain_train_y)
+test_y = regr.predict(test_x)
 
-april_rain_y_pred = regr.predict(april_rain_test_x)
+plt.scatter(may_rain.iloc[np.floor(len(year_cols)*0.7).astype(int):]['year'].values, test_y, label='predicted')
+plt.scatter(may_rain.iloc[np.floor(len(year_cols)*0.7).astype(int):]['year'].values, test_y_truth, label='truth')
+plt.legend(fontsize='x-small')
+plt.xlabel('year')
+plt.ylabel('may rainfall (cm)')
+plt.show()
 
-plt.scatter(april_rain_test.year, april_rain_test_y, label='test')
-plt.scatter(april_rain_test.year, april_rain_y_pred, label='prediction')
+
+all_preds = regr.predict(dF.transpose().values)
+
+rain_to_prod_best_fit_params = np.load('data/lin_reg_best_fit_may_rain_total_production.npy')
+pred_prod = rain_to_prod_best_fit_params[0] * all_preds + rain_to_prod_best_fit_params[1]
+
+plt.scatter(dF.columns.astype(float), pred_prod/125.0, label='predicted')
+plt.scatter(prod.year, prod.weight_kg/125.0, label='truth')
+plt.axhline(28.5, color='k', linewidth=2.0, label='break even')
+plt.xlabel('year')
+plt.ylabel('yearly production (cargas)')
 plt.legend(fontsize='x-small')
 plt.show()
