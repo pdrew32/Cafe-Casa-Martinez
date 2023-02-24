b37499c82b0cd4596ba7dfd85755b30e3f553e33
pdrew32 <patrickmdrew@gmail.com>
2023-02-24 09:00:28 -0500

    added multiple figures

--- save_best_fit_correlations.py
+++ save_best_fit_correlations.py
@@ -5,14 +5,16 @@ import seaborn as sns
 import constants_helper as c
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from matplotlib.ticker import MaxNLocator
+
 
 """
 Fit and save best fit correlations and make some plots using them for the analysis
 """
 
 plot_it = True
-save_fits = True
-save_figures = True
+save_fits = False
+save_figures = False
 
 save_path_may_rain_prod = 'figures/may_rain_vs_production.png'
 save_path_may_rain_prod_per_plant = 'figures/may_rain_vs_production_per_plant.png'
@@ -20,9 +22,17 @@ save_path_n_plants_tot_prod = 'figures/n_plants_total_production.png'
 save_path_median_rain_vs_month = 'figures/median_rainfall_vs_month.png'
 save_path_may_rain_prod_perfect_prediction_with_year = 'figures/may_rain_vs_production_perfect_info_with_year.png'
 save_path_may_rain_prod_perfect_prediction = 'figures/may_rain_vs_production_perfect_info.png'
-
+save_path_year_vs_prod = 'figures/year_vs_production.png'
+save_path_year_vs_prod_per_plant = 'figures/year_vs_prod_per_plant.png'
+save_path_year_vs_prod_projection = 'figures/year_vs_production_projection.png'
+save_path_year_vs_prod_per_plant_projection = 'figures/year_vs_prod_per_plant_projection.png'
+save_path_year_vs_nplant = 'figures/year_vs_nplants.png'
+save_path_field_actions_year_hist = 'figures/field_actions_year_hist.png'
+save_path_renew_year_hist = 'figures/renewal_year_hist.png'
+save_path_sow_year_hist = 'figures/sow_year_hist.png'
 
 tot_plants = pd.read_csv('data/tot_plants.csv', index_col=0)
+lots = pd.read_csv('data/lots.csv', index_col=0)
 
 def lin_reg_params(x, y):
     """
@@ -53,17 +63,126 @@ profit_threshold = c.c.profit_thresh_kg
 x_ = tot_plants.may_rain_cm
 y_ = m_rain_prod*x_ + b_rain_prod
 
+field_actions = np.hstack([lots.cut_year, lots.sow_year])
+
 if plot_it is True:
+    ax = plt.figure().gca()
+    sns.scatterplot(x=tot_plants.year.values, y=tot_plants.total_production.values, s=60)
+    plt.xlabel('Year')
+    plt.ylabel('Total Production (kg)')
+    plt.xticks(rotation=45) # fontsize=12, 
+    # plt.yticks(fontsize=12)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_year_vs_prod)
+    plt.show()
+
+    ax = plt.figure().gca()
+    sns.scatterplot(x=tot_plants.year.values, y=tot_plants.total_production.values, s=60)
+    plt.xlabel('Year')
+    plt.ylabel('Total Production (kg)')
+    plt.xticks(rotation=45) # fontsize=12, 
+    plt.xlim(min(tot_plants.year)-2, 2035)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_year_vs_prod_projection)
+    plt.show()
+
+    ax = plt.figure().gca()
+    plt.hist(field_actions, bins=15)
+    plt.ylabel('Number of Fields Sown or Renewed')
+    plt.xlabel('Year')
+    plt.xlim(min(tot_plants.year), max(tot_plants.year)+1)
+    plt.ylim(0, 5)
+    plt.xticks(rotation=45)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_field_actions_year_hist)
+    plt.show()
+
+    ax = plt.figure().gca()
+    plt.hist(lots.cut_year, color=sns.color_palette('colorblind')[0])
+    plt.ylabel('Number of Fields Renewed')
+    plt.xlabel('Year')
+    plt.ylim(0, 5)
+    plt.xlim(min(tot_plants.year), max(tot_plants.year)+1)
+    plt.xticks(rotation=45)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_renew_year_hist)
+    plt.show()
+
+    ax = plt.figure().gca()
+    plt.hist(lots.sow_year, color=sns.color_palette('colorblind')[2])
+    plt.ylabel('Number of Fields Sown')
+    plt.xlabel('Year')
+    plt.ylim(0, 5)
+    plt.xlim(min(tot_plants.year), max(tot_plants.year)+1)
+    plt.xticks(rotation=45)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_sow_year_hist)
+    plt.show()
+
+    ax = plt.figure().gca()
+    sns.scatterplot(x=tot_plants.year.values, y=tot_plants.prod_per_plant_kg.values, s=60)
+    plt.xlabel('Year')
+    plt.ylabel('Production Per Plant (kg)')
+    plt.xticks(rotation=45) # fontsize=12, 
+    # plt.yticks(fontsize=12)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_year_vs_prod_per_plant)
+    plt.show()
+
+    ax = plt.figure().gca()
+    sns.scatterplot(x=tot_plants.year.values, y=tot_plants.prod_per_plant_kg.values, s=60)
+    plt.xlabel('Year')
+    plt.ylabel('Production Per Plant (kg)')
+    plt.xticks(rotation=45) # fontsize=12, 
+    plt.xlim(min(tot_plants.year)-2, 2035)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_year_vs_prod_per_plant_projection)
+    plt.show()
+
+    ax = plt.figure().gca()
+    sns.scatterplot(x=tot_plants.year, y=tot_plants.tot_plants, s=60)
+    plt.xlabel('Year')
+    plt.ylabel('Total Number of Plants')
+    plt.xticks(rotation=45) # fontsize=12, 
+    # plt.yticks(fontsize=12)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if save_figures is True:
+        plt.savefig(save_path_year_vs_nplant)
+    plt.show()
+
     x0 = np.linspace(0, max(tot_plants.may_rain_cm)+0.1*max(tot_plants.may_rain_cm))
     y0 = m_rain_prod*x0 + b_rain_prod
-    plt.scatter(tot_plants.may_rain_cm, tot_plants.total_production, s=50, color=sns.color_palette('colorblind')[0])
+    fig, ax = plt.subplots()
+    scat = plt.scatter(tot_plants.may_rain_cm, tot_plants.total_production, s=50, c=tot_plants.year)
     plt.axis([plt.gca().get_xlim()[0], plt.gca().get_xlim()[1], plt.gca().get_ylim()[0], plt.gca().get_ylim()[1]])
-    plt.axhline(profit_threshold, linewidth=2, color='red', zorder=-1)
+    # plt.axhline(profit_threshold, linewidth=2, color='red', zorder=-1)
+    cb = fig.colorbar(scat, ax=ax)
+    cb.set_label('year')
     plt.plot(x0, y0, linewidth=2.0, color='k', zorder=-1)
     plt.xlabel('Total Rain in May (mm)')
     plt.ylabel('Total Production (kg)')
     if save_figures is True:
         plt.savefig(save_path_may_rain_prod)
+    plt.show()
+
+    x_hat = tot_plants.may_rain_cm
+    y_hat = m_rain_prod*x_hat + b_rain_prod
+    plt.hist(y_hat-tot_plants.total_production, bins=5)
+    plt.show()
+
+    plt.scatter(x_hat, y_hat-tot_plants.total_production)
+    plt.axhline(-np.std(y_hat-tot_plants.total_production))
+    plt.axhline(np.std(y_hat-tot_plants.total_production))
     plt.show()
 
     fig, ax = plt.subplots()
@@ -114,8 +233,11 @@ if plot_it is True:
 
     x0 = np.linspace(0, max(tot_plants.may_rain_cm)+0.1*max(tot_plants.may_rain_cm))
     y0 = m_rain_prod_per_plant*x0 + b_rain_prod_per_plant
-    plt.scatter(tot_plants.may_rain_cm, tot_plants.prod_per_plant_kg, s=50, color=sns.color_palette('colorblind')[0])
+    fig, ax = plt.subplots()
+    scat = plt.scatter(tot_plants.may_rain_cm, tot_plants.prod_per_plant_kg, s=50, c=tot_plants.year)
     plt.axis([plt.gca().get_xlim()[0], plt.gca().get_xlim()[1], plt.gca().get_ylim()[0], plt.gca().get_ylim()[1]])
+    cb = fig.colorbar(scat, ax=ax)
+    cb.set_label('year')
     plt.plot(x0, y0, linewidth=2.0, color='k', zorder=-1)
     plt.xlabel('Total Rain in May (mm)')
     plt.ylabel('Production Per Plant (kg)')
